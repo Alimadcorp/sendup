a6fb1563103629475f9a048b08ffa219d6b4570b
Alimadcorp <119115454+Alimadcorp@users.noreply.github.com>
2024-11-23 15:41:01 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
-let dates, days, firsubs, secsubs, ranges, mins, maxes, fsubjects, ssubjects, centers;
+let dates,
+  days,
+  firsubs,
+  secsubs,
+  ranges,
+  mins,
+  maxes,
+  fsubjects,
+  ssubjects,
+  centers;
 let ICS1 = "I Comp. Sc. I English I Math I Isl Std. I Urdu I Physics I Al Quran";
 let PE1 = "I Chem I English I Math I Isl Std. I Urdu I Physics I Al Quran";
 let PM1 = "I Chem I English I Biology I Isl Std. I Urdu I Physics I Al Quran";
 let ICS2 = "II Comp. Sc. II English II Math II Pak. St. II Urdu II Physics II Al Quran";
 let PE2 = "II Chem II English II Math II Pak. St. II Urdu II Physics II Al Quran";
 let PM2 = "II Chem II English II Biology II Pak. St. II Urdu II Physics II Al Quran";
-let ICSA1, ICSA2, PEA1, PEA2, PMA1, PMA2;
+
+let ART1 = "I English Lit. I Persian I Punjabi I H. & P. Edu. I Fine Arts I Geog I Islamiyat Elective I History of Pakistan I History of Muslim India I Ethics I Edu I Phil. I Pak Culture I Civics I French I Urdu Advance I Urdu Adv.";
+let ART2 = "II English Lit. II Persian II Punjabi II H. & P. Edu. II Fine Arts II Geog II Islamiyat Elective II History of Pakistan II History of Muslim India II Ethics II Edu II Phil. II Pakistan Culture II Civics II French II Urdu Advance II Urdu Adv. II Psy";
+
+let COM1 = "I Principles of Econ. I Business Math I Principles of Comm. I Princ. of Account.";
+let COM2 = "II Comm. Geo. II Business Math II Principles of Comm. II Princ. of Account. II Statistics";
+let GS1 = "I Econ I Stat";
+let GS2 = "II Economics II Statistics";
+let ICSA1, ICSA2, PEA1, PEA2, PMA1, PMA2, ARTA1, ARTA2, COMA1, COMA2, GSA1, GSA2;
 
 function mapdata() {
   fsubjects = rem(markAsNull(datac[0], " II"));
@@ -24,13 +41,22 @@ function mapschedule() {
   ICSA1 = saparator(ICS1, "I ");
   PEA1 = saparator(PE1, "I ");
   PMA1 = saparator(PM1, "I ");
+  ARTA1 = saparator(ART1, "I ");
+  COMA1 = saparator(COM1, "I ");
+  GSA1 = saparator(GS1, "I ");
   ICSA2 = saparator(ICS2, "II ");
   PEA2 = saparator(PE2, "II ");
   PMA2 = saparator(PM2, "II ");
+  ARTA2 = saparator(ART2, "II ");
+  COMA2 = saparator(COM2, "II ");
+  GSA2 = saparator(GS2, "II ");
 }
 
 function saparator(input, delimiter) {
-  return input.split(delimiter).map((x) => x.trim().replace(/"/g, "")).filter((x) => x);
+  return input
+    .split(delimiter)
+    .map((x) => x.trim().replace(/"/g, ""))
+    .filter((x) => x);
 }
 
 function parseRange(input) {
@@ -58,9 +84,14 @@ function validate(input) {
   err.textContent = "";
   table.style.display = "none";
 
-  if (!input.trim()) return (err.textContent = "Input cannot be empty.", false);
-  if (/[a-zA-Z]/.test(input)) return (err.textContent = "Input cannot contain alphabets.", false);
-  if (!/^\d{1,4}-\d{1,2}(-\d{2})?$/.test(input)) return (err.textContent = 'Invalid format. Please enter in "XXXX-X-XX"', false);
-  if (extract(input)?.grade > 2 || extract(input)?.grade < 1) return (err.textContent = "You must be either 1st year or 2nd year", false);
+  if (!input.trim()) return (err.textContent = "Input cannot be empty."), false;
+  if (/[a-zA-Z]/.test(input))
+    return (err.textContent = "Input cannot contain alphabets."), false;
+  if (!/^\d{1,4}-\d{1,2}(-\d{2})?$/.test(input))
+    return (
+      (err.textContent = 'Invalid format. Please enter in "XXXX-X-XX"'), false
+    );
+  if (extract(input)?.grade > 2 || extract(input)?.grade < 1)
+    return (err.textContent = "You must be either 1st year or 2nd year"), false;
   return true;
 }
