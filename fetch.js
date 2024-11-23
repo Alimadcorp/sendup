1a28e5374bcbbc3a5f82cafe57c2a809b4bc28ec
Alimadcorp <119115454+Alimadcorp@users.noreply.github.com>
2024-11-23 06:20:04 +0500

    Optimize fetch.js

--- fetch.js
+++ fetch.js
@@ -1,45 +1,21 @@
-let schedule; let datac;
-fetch("schedule.csv")
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    return response.text();
-  })
-  .then((data) => {
-    const table = csvToArray(data);
-    const columns = transposeArray(table);
-    schedule = columns;
-    mapschedule();
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+let schedule, datac;
 
-fetch("data.csv")
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    return response.text();
-  })
-  .then((data) => {
-    const table = csvToArray(data);
-    const columns = transposeArray(table);
-    datac = columns;
-    mapdata();
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+const fetchCSV = (url, callback) => {
+  fetch(url)
+    .then((res) => (res.ok ? res.text() : Promise.reject(`HTTP error! Status: ${res.status}`)))
+    .then((data) => callback(transposeArray(csvToArray(data))))
+    .catch(console.error);
+};
 
-function csvToArray(csv) {
-  return csv
-    .trim()
-    .split("\n")
-    .map((row) => row.split(","));
-}
+fetchCSV("schedule.csv", (data) => {
+  schedule = data;
+  mapschedule();
+});
 
-function transposeArray(array) {
-  return array[0].map((_, colIndex) => array.map((row) => row[colIndex]));
-}
+fetchCSV("data.csv", (data) => {
+  datac = data;
+  mapdata();
+});
+
+const csvToArray = (csv) => csv.trim().split("\n").map((row) => row.split(","));
+const transposeArray = (arr) => arr[0].map((_, i) => arr.map((row) => row[i]));
