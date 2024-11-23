b67f6ad09d2de72ae6d4649924fb9cb322e6b84e
Alimadcorp <119115454+Alimadcorp@users.noreply.github.com>
2024-11-23 22:47:57 +0500

    Update generate.js

--- generate.js
+++ generate.js
@@ -105,7 +105,7 @@ async function generateSchedule() {
   localStorage.setItem("gs", gs);
   let rollNoInput = inputElement.value;
   let extras = extra.checked;
-  let logs = "&nbsp&nbsp1.54.2vMS";
+  let logs = "&nbsp&nbsp1.6.1vMS";
   localStorage.setItem("xtra", extra.checked ? "yes" : "no");
   if (validate(rollNoInput)) {
     bb.style.display = "inline";
